b736aec11d442f47414c765544ba71c26fb22652
Roman Permiakov <roman.teckk@gmail.com>
2022-03-03 11:51:16 +0500

    + рефакторинг

--- src/Program.java
+++ src/Program.java
@@ -1,11 +1,9 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 public class Program {
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        long start = System.nanoTime();
         if (args.length < 2){
             throw new IllegalArgumentException("принимаемые аргументы: [URL] [destinationFilePath] [глубина обхода {опционально, по умолчанию равна 1}]");
         }
@@ -18,15 +16,14 @@ public class Program {
         SiteMap map = new SiteMap();
         String siteMap = map.BuildMdSiteMap(url, maxNestedLevel);
         System.out.println(siteMap);
+
         try(FileWriter writer = new FileWriter(destinationPath, false))
         {
             writer.write(siteMap);
             writer.flush();
         }
         catch(IOException ex){
-            System.out.println(ex.getMessage());
+            System.err.printf("Произошла ошибка ввода-вывода\nВы пытались сохранить данные в файл: %s\nТекст ошибки: %s", destinationPath, ex.getMessage());
         }
-        System.out.println(String.format("Executed by %d s",
-                (System.nanoTime() - start) / (1000_000_000)));
     }
 }
